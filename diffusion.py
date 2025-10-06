48a6e33c4df8edbf8c69743fca9fe834d9043920
okembs <Nwekepaul68@gmail.com>
2025-10-05 14:41:42 -1100

    make some changes and learnt alot

---
+++ diffusion.py
@@ -0,0 +1,70 @@
+#  the code for the difusion model 
+from torch import nn 
+import torch 
+from typing import List
+from torch.nn import functional as F
+from  encoder import VaeEncoder
+from decoder import VaeDecoder
+from Clipencoder import CLIP
+from UnetArchitecture import Unet
+from UnetArchitecture import DDPM
+
+
+ 
+class DiffusionModelWrapper(nn.Module) : 
+    def __init__(self , diffusion_model:Unet) : 
+        super().__init__()
+        self.diffusion_model = diffusion_model
+
+    def forward(self , x:torch.Tensor , time_steps:torch.Tensor ,context:torch.Tensor) : 
+        return self.diffusion_model(x , time_steps , context)
+
+#for the latent diffusion model 
+#the unet model will means for the model architecture 
+#the clip embedder will be used for the text embeddings 
+# the latent scaler will be factor used for the latent space
+# n_steps is the number of diffuison steps 
+# linear start is the start of the Beta schedule
+# linear end is the end of the Beta schedule 
+class LatentDiffusionWrapper(nn.Module) : 
+    def __init__(self , unetModel:Unet , autoencoder:VaeEncoder  ,autodecoder:VaeDecoder, clip_embedder:CLIP , latent_scaling_factor:float , n_steps:int , linear_start:float , linear_end:float) : 
+        super().__init__()
+        self.model = DiffusionModelWrapper(unetModel)
+        self.first_stage = autoencoder
+        self.second_stage = autodecoder
+        self.latent_scaling_factor = latent_scaling_factor
+        self.cond_stage = clip_embedder
+        self.n_steps = n_steps 
+        self.linear_start = linear_start
+        self.linear_end = linear_end
+        #self.noise = DDPM(Unet , 10)
+        
+#define the beta schedule
+        beta = torch.linspace(linear_end ** 0.5 , linear_end ** 0.5 , n_steps , dtype=torch.float) ** 2 
+        self.beta = nn.Parameter(beta.to(torch.float32) , requires_grad=False)
+        alpha = 1 - beta 
+
+        alpha_bar = torch.cumprod(alpha ,dim=0)
+        self.aplha_bar = nn.Parameter(alpha_bar.to(torch.float32 ) , requires_grad=False)
+
+ # get the clip embeddings for a list of text prompts
+    def get_text_embedding(self , prompt:List[str]) : 
+        return self.cond_stage(prompt)
+
+ #get the latent space auto encoder of the image 
+ #multiply the latentscalling facror by the autoencoder
+    def  get_latent_autoencoder_encode(self , image:torch.Tensor) :
+        return self.latent_scaling_factor * self.first_stage(image)
+        
+
+  #get the image from the latent space
+    def get_latent_decoder(self, z:torch.Tensor) : 
+        return self.second_stage(z / self.latent_scaling_factor)
+    
+  #predict noise  from the image
+    def forward(self, x:torch.Tensor , t:torch.Tensor , context:torch.Tensor) : 
+        return self.model(x , t ,context)
+
+Okembsmodel = LatentDiffusionWrapper( unetModel=Unet  , autodecoder=VaeDecoder , autoencoder=VaeEncoder , clip_embedder=CLIP  , latent_scaling_factor=2 , n_steps=10 , linear_start=1e-2 , linear_end=2e-1)
+model = DDPM(Okembsmodel)
+print(model)
--- encoder.py
+++ encoder.py
@@ -1,7 +1,9 @@
 from torch import nn 
 import torch 
 from torch.nn import functional as F
-from attentionVariant import SelfAttention
+from attentionVariant import SelfAttention , CrossAttention
+from UnetArchitecture import ResBlock , normalization
+from typing import List
 
 #this will contain the vae encoder 
 
@@ -46,7 +48,7 @@ class VaeEncoder(nn.Module) :
         )
 
 
-    def forward(self , x :torch.tensor,  noise) : 
+    def forward(self , x :torch.tensor, noise) : 
             for module in self : 
                 if getattr(module , 'stride' , None) == (2 ,2):
                     x = F.pad(x , (0 ,1 , 1 , 0)) 
@@ -125,4 +127,183 @@ class VAE_AttentionBlock(nn.Module) :
 
         return x 
     
+
+
+
+#let this be for the autoencoder module 
+#z  : is the number of latent space
+# emb_channels : is the number of the embeddings in the latent space
+#  , z_channels
+class Autoencoder(nn.Module) : 
+    def __init__(self, encoder: 'Encoder', decoder: 'Decoder' , emb_channels:int , z_channels:int):
+        super().__init__()
+        self.encoder = encoder 
+        self.decoder = decoder 
+        self.emb_channels = emb_channels
+        self.z_channels = z_channels
+
+# convolution to map the quantum space to embeddings
+        self.quant_conv = nn.Conv2d(2 * z_channels , 2 * emb_channels , 1)
+# post convolution after the quantum spaced is mapped 
+        self.post_quant_conv = nn.Conv2d(emb_channels , z_channels , 1)
+
+
+# for the encode
+    def encode(self , img:torch.Tensor) : 
+        z = self.encoder(img)
+        moments = self.quant_conv(z)
+        return GaussanDistribution(moments)
+#for the decode : decode images from latent representation
+# z is the latent space with embeddings with batch_size , embsize
+    def  decode(self , z:torch.Tensor) : 
+        z = self.post_quant_conv(z)
+        #decode the image shape
+        return self.decoder(z)
     
+#the encoder contains the channels , 
+# channels_multipliers , 
+# n_res_Block : number of resnet block , 
+# in_channels: number of input channels ,
+#  z_channels : number of the channels in the embedded space
+class Encoder(nn.Module) : 
+    def __init__(self, n_res_Block:int , in_channels:int , z_channels:int , channels_multipliers: List[int] , channels):
+        super().__init__()
+        self.n_res_Block = n_res_Block
+        self.channels = channels
+        self.in_channels = in_channels
+        self.z_channels = z_channels 
+        self.channels_multipliers = channels_multipliers
+
+        n_resolution = len(channels_multipliers)
+        #a convolution layer that maps the image to channel 3 * 3 
+        self.conv_in = nn.Conv2d(in_channels , channels , 3 , padding=1 , stride=1)
+        channel_list = [m * channels for m in [1] + channels_multipliers]
+        self.down = nn.ModuleList( )
+        for i in range(n_resolution) : 
+            resnet_blocks = nn.ModuleList()
+            for _ in range(resnet_blocks) : 
+                resnet_blocks.append(ResnetBlock(channels , channel_list[i + 1]))
+                channels = channel_list[i + 1]
+        down = nn.Module()
+        down.block = resnet_blocks
+        if i != n_resolution - 1 : 
+            down.downsample = DownSample(channels)
+        else : 
+            down.downsample = nn.Identity()
+
+        self.down.append(down)
+
+        #final resnet block with attention
+        self.mid = nn.Module()
+        self.mid_block1 = ResBlock(channels= channels , out_channels=channels , d_temd=None )    # ResnetBlock(channels , channels)
+        self.mid_attn = CrossAttention(320 , 5 , 64 , 64)
+        self.mid_block2 = ResBlock(channels=channels ,out_channels=channels , d_temd=None)
+
+        #map to embeddings space with a 3 x 3 convolution
+        self.norm_out = normalization(channels)
+        self.conv_out = nn.Conv2d(channels , 2 * z_channels , 3 , stride=1, padding=1)
+
+        #img : image tensor with shape
+    def forward(self , img:torch.Tensor) : 
+        x =  self.conv_in(img)
+        for down in self.down : 
+            for block in down.block : 
+                x = block(x)
+
+            x = down.downsample(x)
+            #final resnetblock with attention 
+        x = self.mid_block1(x)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x)
+
+        #normalize and map to the embeddings space 
+        x = self.norm_out(x)
+        x = swish(x)
+        x = self.conv_out(x)
+        return x 
+    
+
+
+
+class Decoder(nn.Module) : 
+    def __init__(self,  n_res_Block:int , in_channels:int , out_channels:int  ,z_channels:int , channels_multipliers: List[int] , channels):
+        super().__init__()
+        self.n_res_Block = n_res_Block
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.z_channels = z_channels
+        self.channels_multipliers = channels_multipliers
+        self.channels = channels
+        
+        #define the rest here
+        n_resolution = len(channels_multipliers)
+        channels_list = [m * channels for m in channels_multipliers]
+        print(channels_list)
+        channels = channels_list[-1]
+        self.conv_in = nn.Conv2d(z_channels , channels , 3 , padding=1 , stride=1)
+
+        #the resnet block with attention 
+        self.mid = nn.Module()
+        self.mid_block1 = ResBlock(channels=channels ,out_channels=channels , d_temd=None)
+        self.mid_attn1 = CrossAttention(320 , 5 , 64, 64)
+        self.mid_block2 = ResBlock(channels=channels , out_channels=channels , d_temd=None)
+        #List of top levels block 
+        self.up = nn.ModuleList()
+        for i in reversed(range(n_resolution)) : 
+            resnet_block =  nn.ModuleList()
+            
+            for _ in range(n_res_Block + 1) : 
+                resnet_block.append(ResBlock(channels=channels , out_channels=channels_list[i] , d_temd=None))
+                channels = channels_list[i]
+
+            up = nn.Module()
+            up.block = resnet_block
+            # uapsmapling each of the block except the first
+            if i != 0 : 
+                up.Upsample = Upsample(channels)
+            else : 
+                up.Upsample = nn.Identity()
+                
+            self.up.insert(0 , up)
+
+            #map to image with a 3 x 3 convolution
+            self.norm_out = normalization(channels)
+            self.conv_out = nn.Conv2d(channels , out_channels , stride=1 , padding=3)
+
+    def forward(self, x:torch.Tensor) : 
+        h = self.conv_in(x)
+        h = self.mid_block1(x)
+        h =  self.mid_attn1(x)
+        h = self.mid_block2(x)
+
+        #Top level blocks
+        for up in reversed(self.up) : 
+            # for the Resnet Block
+            for block in up.block : 
+                h = block(h)        
+            h = Upsample(h)
+     #normalize and map image space 
+        h = self.norm_out(h)
+        h = swish(h)
+        img = self.conv_out(img)
+        return img
+
+# the gaussan distributor
+class GaussanDistribution(nn.Module) : 
+    pass
+
+
+class ResnetBlock : 
+    pass
+
+class DownSample(nn.Module) : 
+    pass
+
+class Upsample(nn.Module) : 
+    pass
+
+
+#for the swish activation 
+# we use the swish activation because it better for optimization than relu
+def swish(x:torch.Tensor) : 
+    return x *  torch.sigmoid(x)
